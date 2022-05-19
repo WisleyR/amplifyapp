0990328ccaa21815f284831a85175176d1d6e2f3
WisleyR <wisley@tangentsolutions.co.za>
2022-05-19 15:04:36 +0200

    changes 2 for wisley

--- src/App.js
+++ src/App.js
@@ -6,17 +6,18 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="Hello from Wisley"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+        <h1>Hello from Wisley </h2>
+
+
+
+
+
+
+
+
+
+
       </header>
     </div>
   );
