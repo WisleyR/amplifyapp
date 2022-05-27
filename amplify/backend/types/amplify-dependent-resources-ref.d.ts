3f26faa3532cd58f1a891ba0cee52ee3f95c395f
WisleyR <wisley@tangentsolutions.co.za>
2022-05-27 10:04:45 +0200

    added an authenticator

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "wesreactapp48d3e799": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    }
+}
